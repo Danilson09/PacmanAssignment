ff2d4ead212f240971b4321395dd8004029a4b4b
Danilson09 <56113775+Danilson09@users.noreply.github.com>
2019-11-28 00:24:00 +0100

    Play area done
    The play area and the prefabs are all done

---
+++ PACMAN/Assets/points.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class points : MonoBehaviour
+{
+    void OnTriggerEnter()
+    {
+        Destroy(gameObject);
+    }
+
+}
