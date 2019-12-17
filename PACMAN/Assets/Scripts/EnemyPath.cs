44e03e21fa431de2a08f8dc7694b33791b346b24
Danilson09 <56113775+Danilson09@users.noreply.github.com>
2019-12-17 22:38:48 +0000

    Attemt in making enemy move

---
+++ PACMAN/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyPath : MonoBehaviour {
+
+ // Array of waypoints to walk from one to the next one
+    [SerializeField]
+    private Transform[] path1;
+
+    // Walk speed that can be set in Inspector
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    // Index of current waypoint from which Enemy walks
+    // to the next one
+    private int path1Index = 0;
+
+	// Use this for initialization
+	private void Start () {
+
+        // Set position of Enemy as position of the first waypoint
+        transform.position = path1[path1Index].transform.position;
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+
+        // Move Enemy
+        Move();
+	}
+
+    // Method that actually make Enemy walk
+    private void Move()
+    {
+        // If Enemy didn't reach last waypoint it can move
+        // If enemy reached last waypoint then it stops
+        if (path1Index <= path1.Length - 1)
+        {
+
+            // Move Enemy from current waypoint to the next one
+            // using MoveTowards method
+            transform.position = Vector2.MoveTowards(transform.position,
+               path1[path1Index].transform.position,
+               moveSpeed * Time.deltaTime);
+
+            // If Enemy reaches position of waypoint he walked towards
+            // then waypointIndex is increased by 1
+            // and Enemy starts to walk to the next waypoint
+            if (transform.position == path1[path1Index].transform.position)
+            {
+               path1Index += 1;
+            }
+        }
+    }
+}
