71050550acd2787ea892ca3f064b082f85655131
Danilson09 <56113775+Danilson09@users.noreply.github.com>
2019-12-17 22:07:09 +0000

    Setting up the Paths
    created 3 paths and moved the prefabs

--- PACMAN/Assets/Scripts/prefab4.cs
+++ PACMAN/Assets/Scripts/prefab4.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class prefab4 : MonoBehaviour
 {
+    [SerializeField] List<Transform> waypoints;
+
+    [SerializeField] float enemySpeed = 12f;
+
+    int waypointIndex = 0;
    
     // Start is called before the first frame update
     void Start()
@@ -15,7 +20,29 @@ public class prefab4 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMovement();
     }
   
+    private void EnemyMovement()
+    {
+        
+            //set the targetPosition to the position of our next wayPoint
+            Vector3 targetPosition = waypoints[waypointIndex].transform.position;
+            targetPosition.z = 0f;
+
+            float movementThisFrame = enemySpeed * Time.deltaTime;
+            
+            //move the enemy from its current position towards the targetPosition
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            
+          
+            
+
+        
+        //if enemy moved to all waypoints
+      
+
+
+    }
 }
